c7d8af4fe1462e5753bee83b4e47929c2920754b
Pankaj Kumar <pankaj3359kr@gmail.com>
2024-09-01 23:21:30 +0530

    make the title unique

--- models/Course.js
+++ models/Course.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const courseSchema = new mongoose.Schema({
-    title: { type: String, required: true },
+    title: { type: String, required: true, unique: true },
     description: { type: String, required: true },
     instructor: { type: String, required: true },
     duration: { type: String, required: true },
